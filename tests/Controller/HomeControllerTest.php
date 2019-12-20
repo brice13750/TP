d0f563aa53d53830ad3ac3c2dae4419e0d47cd67
brice <brice.ouaali@gmail.com>
2019-12-20 16:01:21 +0100

    test ok

--- tests/Controller/HomeControllerTest.php
+++ tests/Controller/HomeControllerTest.php
@@ -8,10 +8,10 @@ class HomeControllerTest extends WebTestCase
 {
     public function testHomepage()
     {
-        //on crée un client (navigateur)
+        //on crée un client HTTP (navigateur)
         $client = static::createClient();
 
-        // on exécute la requête avec la méthode GET sur l'url
+        // on exécute la requête HTTP avec la méthode GET sur l'url
         $client->request('GET', '/');
 
         //on vérifie que le status de la réponse est bien 200 
